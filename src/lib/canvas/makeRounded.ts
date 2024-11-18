e0b3c5851d11c1c92808d96c9d4b22222ece4760
Kiizuha Kanazawa <kiizuha@gnuweeb.org>
2024-11-18 15:24:56 +0700

    refactor(makeRounded): change rounded radius
    Signed-off-by: Kiizuha Kanazawa <kiizuha@gnuweeb.org>

--- src/lib/canvas/makeRounded.ts
+++ src/lib/canvas/makeRounded.ts
@@ -3,7 +3,7 @@ import { CanvasRenderingContext2D } from 'canvas';
 export const makeCanvasRounded = (ctx: CanvasRenderingContext2D, width: number, height: number) => {
   ctx.save();
   ctx.beginPath();
-  ctx.roundRect(0, 0, width, height, [40]);
+  ctx.roundRect(0, 0, width, height, [20]);
   ctx.clip();
 };
 
