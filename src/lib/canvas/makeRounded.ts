aeac1836b9d7cf3d3941081f56cc1cc1409e656e
Kiizuha Kanazawa <kiizuha@gnuweeb.org>
2024-11-18 14:00:09 +0700

    feat(lib): add makeAvatarRounded()
    Signed-off-by: Kiizuha Kanazawa <kiizuha@gnuweeb.org>

--- src/lib/canvas/makeRounded.ts
+++ src/lib/canvas/makeRounded.ts
@@ -6,3 +6,13 @@ export const makeCanvasRounded = (ctx: CanvasRenderingContext2D, width: number, 
   ctx.roundRect(0, 0, width, height, [40]);
   ctx.clip();
 };
+
+export const makeAvatarRounded = (
+  ctx: CanvasRenderingContext2D,
+  { x, y, radius }: { x: number; y: number; radius: number }
+) => {
+  ctx.save();
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+  ctx.clip();
+};
