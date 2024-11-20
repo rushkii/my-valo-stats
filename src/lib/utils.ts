8b47d2513d73a39b18eb41b287dcd472f4842b8d
Kiizuha Kanazawa <kiizuha@gnuweeb.org>
2024-11-20 11:02:14 +0700

    feat(lib): add toHumanTime()
    Signed-off-by: Kiizuha Kanazawa <kiizuha@gnuweeb.org>

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,19 @@
 export const getRrankImage = (rank: string) => {
   return `./src/assets/images/ranks/${rank.toLowerCase().replace(' ', '_')}_small.png`;
 };
+
+export const toHumanTime = (millis: number) => {
+  const pad = (num: number) => {
+    return num.toString().padStart(2, '0');
+  };
+
+  const totalSeconds = Math.floor(millis / 1000);
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+  const seconds = totalSeconds % 60;
+
+  let result = hours > 0 ? hours + ':' : '';
+  result = `${pad(minutes)}:${pad(seconds)}`;
+
+  return result;
+};
