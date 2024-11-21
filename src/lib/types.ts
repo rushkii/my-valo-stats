1942fdb9e3d49ec49b760c3464e87ccffc5c7b8a
Kiizuha Kanazawa <kiizuha@gnuweeb.org>
2024-11-21 16:57:46 +0700

    feat(types): add MatchType and ParticipantType
    Signed-off-by: Kiizuha Kanazawa <kiizuha@gnuweeb.org>

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -13,3 +13,39 @@ export interface DrawRoundedRectOpts {
   height: number;
   radius: number;
 }
+
+export interface MatchType {
+  matchId: string;
+  queueType: string;
+  queueTypeLoc: string;
+  gameLengthMillis: number;
+  gameStartMillis: number;
+  matchResult: string;
+  mapTitleLoc: string;
+  mapBackground: string;
+  mapBackgroundSplash: string;
+  wins: number;
+  loses: number;
+  participants: ParticipantType[];
+  platformType: string;
+}
+
+export interface ParticipantType {
+  playerPUUID: string;
+  playerName: string;
+  rank: {
+    rank: number;
+    rankName: string;
+  };
+  accountLevel: number;
+  agentIcon: string;
+  agentNameLoc: string;
+  team: string;
+  kda: string;
+  score: number;
+  econRating: number;
+  plants: number;
+  defuses: number;
+  firstBlood: number;
+  isExpandable: boolean;
+}
