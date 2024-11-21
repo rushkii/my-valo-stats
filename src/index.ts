9b66690d5b9c2f0b6b63e197f3b187602df11fb4
Kiizuha Kanazawa <kiizuha@gnuweeb.org>
2024-11-21 17:02:46 +0700

    feat: call resetOutputs(), generateMatches() and rewriteMatchHistory()
    Signed-off-by: Kiizuha Kanazawa <kiizuha@gnuweeb.org>

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-import { generateProfileCard } from './generate';
+import { generateMatches, generateProfileCard } from './generate';
+import { resetOutputs, rewriteMatchHistory } from './lib/utils';
 
-const main = () => {
-  generateProfileCard();
+const main = async () => {
+  resetOutputs();
+  await Promise.all([generateProfileCard(), generateMatches()]);
+  rewriteMatchHistory();
 };
 
 if (require.main === module) {
