12a671297e013408fb113daf00f8c61d1d0d867d
Kiizuha Kanazawa <kiizuha@gnuweeb.org>
2024-11-22 17:52:28 +0700

    feat(utils): add getElementObject() and loadElementFromString()
    Signed-off-by: Kiizuha Kanazawa <kiizuha@gnuweeb.org>

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,8 @@
 import { readFileSync, rmSync, writeFileSync } from 'fs';
 import { globSync } from 'glob';
+import * as cheerio from 'cheerio';
+import { Element } from 'domhandler';
+import { ContentElement, ExtractedElement } from './types';
 
 export const getRrankImage = (rank: string) => {
   return `./src/assets/images/ranks/${rank.toLowerCase().replace(' ', '_')}_small.png`;
@@ -48,4 +51,37 @@ export const rewriteMatchHistory = () => {
 
 export const resetOutputs = () => {
   rmSync('output', { recursive: true, force: true });
+};
+
+const getElementObject = (
+  root: cheerio.Cheerio<Element>,
+  $: cheerio.CheerioAPI
+): ExtractedElement => {
+  const elementName = root[0].tagName;
+  const attributes = root.attr();
+  const children = root.contents();
+
+  const childrenContent: ContentElement[] = [];
+
+  children.each((_, child) => {
+    if (child.type === 'tag') {
+      const childElement = $(child);
+      childrenContent.push(getElementObject(childElement, $));
+    } else if (child.type === 'text') {
+      const text = child.data.trim();
+      if (text) childrenContent.push(text);
+    }
+  });
+
+  return {
+    tagName: elementName,
+    attributes: attributes || {},
+    content: childrenContent
+  };
+};
+
+export const loadElementFromString = (element: string) => {
+  const $ = cheerio.load(element);
+  const root = $('span').first();
+  return getElementObject(root, $);
 };
