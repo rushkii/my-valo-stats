5ecb2df7e62779c16c2ea45a5d4c6af7e60b8e21
Kiizuha Kanazawa <kiizuha@gnuweeb.org>
2024-11-24 23:06:10 +0700

    fix(canvas): fix responsiveness for writeTextWithRounded()
    Signed-off-by: Kiizuha Kanazawa <kiizuha@gnuweeb.org>

--- src/lib/canvas/writeTextWithRounded.ts
+++ src/lib/canvas/writeTextWithRounded.ts
@@ -1,5 +1,5 @@
 import { CanvasRenderingContext2D } from 'canvas';
-import { MeasureType, WriteTextWithRoundedOpts } from '../types';
+import { WriteTextWithRoundedOpts } from '../types';
 import { drawRoundedRect } from './roundRect';
 
 export const writeTextWithRounded = (
@@ -18,53 +18,88 @@ export const writeTextWithRounded = (
   }: WriteTextWithRoundedOpts
 ) => {
   ctx.save();
-  ctx.beginPath();
 
   ctx.font = font;
   ctx.textAlign = align;
   ctx.textBaseline = baseline;
 
-  // text calculation for responsive positions
-  const metrics: MeasureType = ctx.measureText(text);
+  const fontSize = parseInt(font);
+
+  const metrics = ctx.measureText(text);
   const textWidth = metrics.width;
-  const textHeight = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
-
-  let alignCalculation = 0;
-  let baselineCalculation = 0;
-
-  if (align === 'center') {
-    alignCalculation = metrics.actualBoundingBoxRight + metrics.emHeightDescent! + 2;
-  } else if (align === 'left' || align === 'start') {
-    alignCalculation = metrics.actualBoundingBoxAscent;
-  } else if (align === 'right' || align === 'end') {
-    alignCalculation = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight - 6;
-  }
-
-  if (baseline === 'alphabetic') {
-    baselineCalculation = 9 - metrics.alphabeticBaseline!;
-    alignCalculation -= 0;
-  } else if (baseline === 'bottom' || baseline === 'middle') {
-    baselineCalculation = metrics.alphabeticBaseline! + 8;
-    alignCalculation -= 4;
-  } else if (baseline === 'hanging' || baseline === 'ideographic' || baseline === 'top') {
-    baselineCalculation = metrics.alphabeticBaseline! + 8;
-    alignCalculation -= 14;
-  }
+  const textAscent = metrics.actualBoundingBoxAscent || metrics.fontBoundingBoxAscent || fontSize;
+  const textDescent =
+    metrics.actualBoundingBoxDescent || metrics.fontBoundingBoxDescent || fontSize * 0.2;
+  const textHeight = textAscent + textDescent;
 
   const rectWidth = textWidth + padding * 2;
   const rectHeight = textHeight + padding * 2;
 
+  let rectX = x;
+  switch (align) {
+    case 'start':
+    case 'left':
+      rectX = x;
+      break;
+    case 'end':
+    case 'right':
+      rectX = x - rectWidth;
+      break;
+    case 'center':
+      rectX = x - rectWidth / 2;
+      break;
+  }
+
+  let rectY = y;
+  let textY = y;
+
+  switch (baseline) {
+    case 'alphabetic':
+      rectY = y - textAscent - padding;
+      break;
+    case 'top':
+    case 'hanging':
+      rectY = y;
+      textY = y + padding;
+      break;
+    case 'middle':
+      rectY = y - rectHeight / 2;
+      break;
+    case 'bottom':
+    case 'ideographic':
+      rectY = y - rectHeight;
+      textY = y - padding;
+      break;
+  }
+
+  const maxRadius = Math.min(radius, rectWidth / 2, rectHeight / 2);
+
   ctx.fillStyle = backgroundColor;
   drawRoundedRect(ctx, {
-    x: x - alignCalculation,
-    y: y - padding - baselineCalculation,
-    width: rectWidth - 3,
-    height: rectHeight - 3,
-    radius
+    x: rectX,
+    y: rectY,
+    width: rectWidth,
+    height: rectHeight,
+    radius: maxRadius
   });
 
+  let textX = x;
+  switch (align) {
+    case 'start':
+    case 'left':
+      textX = rectX + padding;
+      break;
+    case 'end':
+    case 'right':
+      textX = rectX + rectWidth - padding;
+      break;
+    case 'center':
+      textX = rectX + rectWidth / 2;
+      break;
+  }
+
   ctx.fillStyle = textColor;
-  ctx.fillText(text, x, y);
+  ctx.fillText(text, textX, textY);
 
   ctx.restore();
 };
